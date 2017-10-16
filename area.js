7c2c7214e00f214645fc3b9d9a9edb9879882829
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-10-16 14:47:13 +0900

    다각형을 계산하는 함수

--- area.js
+++ area.js
@@ -1,7 +1,24 @@
-var circle = Math.PI * 10 * 10;
-var square = 10 * 15;
-var trapezoid = ((10 + 5) / 2) * 12;
+const getCircle = function (...theArgs) {
+    if (theArgs.length !== 1) {
+        return "1개의 인자가 필요합니다";
+    }
+    let radius = theArgs[0];
 
-console.log(circle);
-console.log(square);
-console.log(trapezoid);
+    return Math.PI * radius * radius;
+}
+const getSquare = function (...theArgs) {
+    if (theArgs.length !== 2) {
+        return "2개의 인자가 필요합니다";
+    }
+    let [width, height] = theArgs;
+
+    return width * height;
+}
+const getTrapezoid = function (...theArgs) {
+    if (theArgs.length !== 3) {
+        return "3개의 인자가 필요합니다";
+    }
+    let [base, upper, height] = theArgs;
+
+    return ((base + upper) / 2) * height;
+}
