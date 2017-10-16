39faca3b1879a5f2cb446f1c3c670054e09f2dbb
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-10-16 15:02:21 +0900

    파라미터를 체크하는 다격형의 넓이 함수

--- area.js
+++ area.js
@@ -1,23 +1,59 @@
-const getCircle = function (...theArgs) {
+const calculateCircle = function (...theArgs) {
+    if (theArgs.length === 0) {
+        return "최소 한가지 값이 필요합니다";
+    }
     if (theArgs.length !== 1) {
         return "1개의 인자가 필요합니다";
     }
+    for (let i = 0, len = theArgs.length; i < len; i++) {
+        if (!Number.isInteger(theArgs[i])) {
+            return "숫자형타입만 계산이 가능합니다.";
+        }
+    }
+    if (theArgs[0] < 0)
+        return "반지름은 0보다 커야 합니다";
     let radius = theArgs[0];
 
     return Math.PI * radius * radius;
 }
-const getSquare = function (...theArgs) {
+const calculateSquare = function (...theArgs) {
+    if (theArgs.length === 0) {
+        return "최소 한가지 값이 필요합니다";
+    }
     if (theArgs.length !== 2) {
         return "2개의 인자가 필요합니다";
     }
+    for (let i = 0, len = theArgs.length; i < len; i++) {
+        if (!Number.isInteger(theArgs[i])) {
+            return "숫자형타입만 계산이 가능합니다.";
+        }
+    }
+    if (theArgs[0] < 0)
+        return "밑변은 0보다 커야 합니다";
+    if (theArgs[1] < 0)
+        return "높이는 0보다 커야 합니다";
     let [width, height] = theArgs;
 
     return width * height;
 }
-const getTrapezoid = function (...theArgs) {
+const calculateTrapezoid = function (...theArgs) {
+    if (theArgs.length === 0) {
+        return "최소 한가지 값이 필요합니다";
+    }
     if (theArgs.length !== 3) {
         return "3개의 인자가 필요합니다";
     }
+    for (let i = 0, len = theArgs.length; i < len; i++) {
+        if (!Number.isInteger(theArgs[i])) {
+            return "숫자형타입만 계산이 가능합니다.";
+        }
+    }
+    if (theArgs[0] < 0)
+        return "밑변은 0보다 커야 합니다";
+    if (theArgs[1] < 0)
+        return "윗변은 0보다 커야 합니다";
+    if (theArgs[2] < 0)
+        return "높이는 0보다 커야 합니다";
     let [base, upper, height] = theArgs;
 
     return ((base + upper) / 2) * height;
