60b13e8f4146d3103e0bd127d23b880d03314cc5
HYEON(mac) <antaehyeon@live.co.kr>
2018-03-08 14:08:22 +0900

    다각형의 넓이 중 배열 강의정리 추가(+filter, reduce, some, every)

--- PolygonWidth/function.js
+++ PolygonWidth/function.js
@@ -1,18 +1,39 @@
-// 함수의 호출.
-function printName(firstname) {
-    var myname = "jisu";
-    return myname + ", " +  firstname;
-}
+// var origin = [1,2,3,4];
+// var changedArray = [...origin, 2, 3];
 
-function run(firstname) {
-   var firstname = firstname || "Youn";
-   var result = printName(firstname);
-   console.log(result);
-}
+// changedArray.forEach(function(v, i, o) {
+//     console.log(v);
+// })
 
-function excute() {
-    run('Kim');
-    printName();
-}
+// var mapped = changedArray.map(function(v){ 
+//     return v * 2; // changedArray의 원소를 돌면서, 값을 변경시킨 후 새로운 배열로 만들어서 리턴
+// })
 
-excute();
+// console.log(changedArray);
+// console.log(mapped);
+
+
+
+// function isBigEnough(value) {
+//     return value >= 10;
+//   }
+//   var filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+
+//   console.log(filtered);
+//   // filtered 는 [12, 130, 44]
+
+//   var filtered = [12, 5, 8, 130, 44].filter(function isBigEnough(value){
+//       return value >= 10;
+//   });
+
+//   console.log(filtered);
+
+
+var total = [0, 1, 2, 3].reduce(function(a, b) {
+    return a + b;
+  });
+
+var total2 = [0, 1, 2, 3].reduce(elem => elem + 10)
+
+console.log(total);
+console.log(total2);
