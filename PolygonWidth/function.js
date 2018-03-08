349444e80a1000e257056f6b0598d51eaf8dba4e
HYEON(mac) <antaehyeon@live.co.kr>
2018-03-08 17:01:44 +0900

    최대공약수 퀴즈 반복문 이용하여 코드작성

--- PolygonWidth/function.js
+++ PolygonWidth/function.js
@@ -1,39 +1,15 @@
-// var origin = [1,2,3,4];
-// var changedArray = [...origin, 2, 3];
+function gcd(x, y) {
+  let i = 0;
+  let result = 0;
+  
+  while(true) {
+    i++;
+    if (i === y || i === x) return result;
+    if ((x % i === 0) && (y % i === 0)) {
+      result = i;
+    } // if
+  } // while
+} // function
 
-// changedArray.forEach(function(v, i, o) {
-//     console.log(v);
-// })
-
-// var mapped = changedArray.map(function(v){ 
-//     return v * 2; // changedArray의 원소를 돌면서, 값을 변경시킨 후 새로운 배열로 만들어서 리턴
-// })
-
-// console.log(changedArray);
-// console.log(mapped);
-
-
-
-// function isBigEnough(value) {
-//     return value >= 10;
-//   }
-//   var filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
-
-//   console.log(filtered);
-//   // filtered 는 [12, 130, 44]
-
-//   var filtered = [12, 5, 8, 130, 44].filter(function isBigEnough(value){
-//       return value >= 10;
-//   });
-
-//   console.log(filtered);
-
-
-var total = [0, 1, 2, 3].reduce(function(a, b) {
-    return a + b;
-  });
-
-var total2 = [0, 1, 2, 3].reduce(elem => elem + 10)
-
-console.log(total);
-console.log(total2);
+console.log(gcd(192, 72));
+console.log(gcd(72, 192));
