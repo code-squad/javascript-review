22e79d03cf1bb947fdc9f73e7d4b8f76b1ca4c1a
HYEON(win) <antaehyeon@live.co.kr>
2018-03-12 11:34:28 +0900

    [Fix] '최대공약수 퀴즈' 결과값 에러를 수정한다
    결과값을 리턴하는 조건문이 먼저 수행되었고,
    예전으로 나눈 값이 결과값에 저장되어 정상적으로 출력되지 않았습니다.

--- PolygonWidth/function.js
+++ PolygonWidth/function.js
@@ -4,12 +4,14 @@ function gcd(x, y) {
   
   while(true) {
     i++;
-    if (i === y || i === x) return result;
     if ((x % i === 0) && (y % i === 0)) {
       result = i;
     } // if
+    if (i === y || i === x) return result;
   } // while
 } // function
 
 console.log(gcd(192, 72));
 console.log(gcd(72, 192));
+console.log(gcd(10,20));
+console.log(gcd(532,462));
