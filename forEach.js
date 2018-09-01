95a352dcfec9158d36e2dbeca5b24b38d7602bf5
GabinKim <gav0910@naver.com>
2018-09-01 17:46:51 +0900

    1주차 객체실습 - 피드백 사항 반영 (1차)

--- forEach.js
+++ forEach.js
@@ -2,6 +2,7 @@
 const firstProps = [];
 const numberProps = [];
 
+// 객체 배열화
 Object.keys(widget).forEach( (key) => {
     if (typeof(widget[key]) == "string") return;
     firstProps.push(widget[key]);
@@ -9,6 +10,7 @@ Object.keys(widget).forEach( (key) => {
 
 firstProps.map((obj, index) => {
     for ( val in obj ){
+        // 프로퍼티의 밸류가 'number' 인지 검증. number 타입일 경우 numberProps 배열에 push.
         if (typeof(obj[val]) == 'number' ) {
             numberProps.push(val);
         }
