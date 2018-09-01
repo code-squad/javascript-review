fcb224644f2aa85db33763bf57bf86863e8edf8c
DESKTOP-6K7TNUG\kt <this1mg@gmail.com>
2018-09-01 17:56:25 +0900

    두번째

--- test.js
+++ test.js
@@ -1,1 +1,40 @@
-// dd
+const widget = {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": { 
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+    };
+    let numbers =[];
+    for( let prop in widget) {
+        let target = widget[ prop];
+
+        if( toString.call( target) === "[object Object]"){
+            for( let prop2 in target) {
+                if( toString.call( target[prop2]) === "[object Number]") numbers.push( prop2);
+            }
+        }else if( toString.call( target) === "[object Number]") numbers.push( prop2);
+
+    }
+    
+    console.log( numbers);
+    
