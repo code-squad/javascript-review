d4c0487f3643cce4a15193a3c4eb738a65ef2ab3
Nam Won Hyung <nwh0526@gmail.com>
2018-09-02 23:07:27 +0900

    codesquad 객체 실습2 commit

---
+++ objectStep2.js
@@ -0,0 +1,96 @@
+const data = [{
+	"id": 1,
+	"name": "Yong",
+	"phone": "010-0000-0000",
+	"type": "sk",
+	"childnode": [{
+		"id": 11,
+		"name": "echo",
+		"phone": "010-0000-1111",
+		"type": "kt",
+		"childnode": [{
+				"id": 115,
+				"name": "hary",
+				"phone": "211-1111-0000",
+				"type": "sk",
+				"childnode": [{
+					"id": 1159,
+					"name": "pobi",
+					"phone": "010-444-000",
+					"type": "kt",
+					"childnode": [{
+							"id": 11592,
+							"name": "cherry",
+							"phone": "111-222-0000",
+							"type": "lg",
+							"childnode": []
+						},
+						{
+							"id": 11595,
+							"name": "solvin",
+							"phone": "010-000-3333",
+							"type": "sk",
+							"childnode": []
+						}
+					]
+				}]
+			},
+			{
+				"id": 116,
+				"name": "kim",
+				"phone": "444-111-0200",
+				"type": "kt",
+				"childnode": [{
+					"id": 1168,
+					"name": "hani",
+					"phone": "010-222-0000",
+					"type": "sk",
+					"childnode": [{
+						"id": 11689,
+						"name": "ho",
+						"phone": "010-000-0000",
+						"type": "kt",
+						"childnode": [{
+								"id": 116890,
+								"name": "wonsuk",
+								"phone": "010-000-0000",
+								"type": "kt",
+								"childnode": []
+							},
+							{
+								"id": 1168901,
+								"name": "chulsu",
+								"phone": "010-0000-0000",
+								"type": "sk",
+								"childnode": []
+							}
+						]
+					}]
+				}]
+			},
+			{
+				"id": 117,
+				"name": "hong",
+				"phone": "010-0000-0000",
+				"type": "lg",
+				"childnode": []
+			}
+		]
+	}]
+}]
+
+// type 속성의 값이 "sk"인 name 값 배열만 출력하자.
+
+// childnode 속성은 값으로 배열을 갖는다. 배열안의 값은 같은 구조를 가진다. (배열 -> 객체
+// 배열안에는 object 속성을 여러개 갖을 수 있다.
+const nameArr = [];
+var extractNameOfTypeIsSK = (data) => {
+    if((typeof data === "undefined") && (data.length === 0)) return;
+
+    data.forEach((obj) => {
+        if(obj.type === 'sk') nameArr.push(obj.name);
+        if(obj.childnode.length !== 0) extractNameOfTypeIsSK(obj.childnode);
+    });
+}
+extractNameOfTypeIsSK(data);
+console.log(nameArr);
