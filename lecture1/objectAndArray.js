336bad8d24276a0e7f97a8f6834ddbd90f40668d
mayjungheelee <may.lee@woowahan.com>
2018-09-08 13:53:11 +0900

    refactor: lint error fix - indent - delete `console.log` see https://eslint.org/docs/rules/no-debugger

--- lecture1/objectAndArray.js
+++ lecture1/objectAndArray.js
@@ -6,7 +6,7 @@ const widget = {
         "width": 500,
         "height": 500
     },
-    "image": { 
+    "image": {
         "src": "Images/Sun.png",
         "name": "sun1",
         "hOffset": 250,
@@ -23,24 +23,17 @@ const widget = {
         "alignment": "center",
         "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
     }
-}
-
+};
 
 const numberKey = [];
 Object.keys(widget).forEach(item => {
-    if (typeof widget[item] === 'number') {
-        numberKey.push(item);
-    } 
-    
-    if (typeof widget[item] === 'object') {
-		const keysInWidget = Object.keys(widget[item]);
+    if (typeof widget[item] === "object") {
+        const keysInWidget = Object.keys(widget[item]);
 
-		keysInWidget.forEach((i) => {
-            if (typeof widget[item][i] === 'number') {
+        keysInWidget.forEach((i) => {
+            if (typeof widget[item][i] === "number") {
                 numberKey.push(i);
-            }	
-		});
+            }
+        });
     }
 });
-
-console.log(numberKey);
